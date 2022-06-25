a80dea448c20096cba583a53f2bda26133cb24a0
Sergey <metrolog_omsk@mail.ru>
2022-06-25 12:16:41 +0500

    Решил задачу № 25 другим способом

---
+++ Task25_Ver2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два натуральных числа (A и B) и возводит число A в степень B.
+
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Рассчитаем результат возведения случайных чисел A в степень B");
+
+LevelUp();
+LevelUp();
+LevelUp();
+
+void LevelUp()
+{
+    Random rand = new Random();
+
+    double number1 = rand.Next(1, 10);
+    double number2 = rand.Next(1, 10);
+    double result = number1;
+    int count = 1;
+    while (count < number2)
+    {
+        result = result * number1;
+        count++;
+    }
+
+    Console.WriteLine($"{number1} в степени {number2} -> {result}");
+}
+
+Console.WriteLine("********************************************************");
