d2173ca4717f38b0a975c5288f8acb2c4811238f
Sergey <metrolog_omsk@mail.ru>
2022-06-25 10:35:38 +0500

    Решил задачу № 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -4,3 +4,24 @@
 // 3, 5 -> 243
 // 2, 4 -> 16
 
+Console.Clear();
+Console.WriteLine("Рассчитаем результат возведения случайных чисел A в степень B");
+Console.WriteLine("Введите количество циклов нахождения результата");
+int count = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    LevelUp();
+}
+
+void LevelUp()
+{
+    Random rand = new Random();
+
+    double number1 = rand.Next(1, 10);
+    double number2 = rand.Next(1, 10);
+    double result = Math.Pow(number1, number2);
+    Console.WriteLine($"{number1} в степени {number2} -> {result}");
+}
+
+Console.WriteLine("********************************************************");
