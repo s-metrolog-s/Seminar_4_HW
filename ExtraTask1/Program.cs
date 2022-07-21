4c03e3cc492cd5dda1cc0771a9298fbb67097007
Sergey <metrolog_omsk@mail.ru>
2022-07-21 21:08:19 +0500

    Решил доп. задачу № 1 (двоичный палиндром)

--- ExtraTask1/Program.cs
+++ ExtraTask1/Program.cs
@@ -1,20 +1,51 @@
 ﻿// Задача 1. На вход подаётся натуральное десятичное число. 
 // Проверьте, является ли оно палиндромом в двоичной записи.
 
+Random rand = new Random();
+int number1 = 17;
+int number2 = 15;
+int number3 = rand.Next(0, 257);
+
 Console.Clear();
-Console.WriteLine("*************************************");
-int number = 10;
-int[] array = new int[];
+Console.WriteLine("******************************************");
+Console.WriteLine($"Даны следующие числа {number1}, {number2}, {number3}");
+Console.WriteLine("В двоичной системе будут представлено как:");
 
-Console.WriteLine(MakeDualFromTen(number));
+Console.Write($"{number1} -> ");
+CheckPalindrom(MakeStringFromNumber(number1));
+Console.Write($"{number2} -> ");
+CheckPalindrom(MakeStringFromNumber(number2));
+Console.Write($"{number3} -> ");
+CheckPalindrom(MakeStringFromNumber(number3));
 
-string MakeDualFromTen(int number)
+Console.WriteLine("******************************************");
+
+string MakeStringFromNumber (int number)
 {
-    string numberDual = string.Empty;
+    string valueToString = String.Empty;
     for (int i = 0; number > 0; i++)
     {
-        numberDual[i] = Convert.ToString(number % 2);
-        number = number / 2;
+        if (number % 2 == 1) valueToString = "1" + valueToString;
+        else valueToString = "0" + valueToString;
+        number /= 2;
     }
-    return numberDual;
+    return valueToString;
 }
+
+void CheckPalindrom (string number)
+{
+    int count = number.Length - 1;
+    bool flag = true;
+    for (int i = 0; i < number.Length / 2; i++)
+    {
+        if (number[i] != number[count])
+        {
+            flag = false;
+            break;
+        }
+        count--;
+    }
+    if (flag) Console.WriteLine($"{number} -> является палиндромом");
+    else Console.WriteLine($"{number} -> не является палиндромом");
+}
+
