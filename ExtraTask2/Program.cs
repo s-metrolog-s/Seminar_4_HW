6fea7935871b66b75c39fcf727fc3b128e7a1a5e
Sergey <metrolog_omsk@mail.ru>
2022-07-25 14:31:47 +0500

    Решил доп. задачу № 2 (заполнение массив 0 и 1)

--- ExtraTask2/Program.cs
+++ ExtraTask2/Program.cs
@@ -5,13 +5,28 @@ Random rand = new Random();
 int numberOnes = rand.Next(1, 50);
 int length = numberOnes * numberOnes;
 int[] currentArray = new int[length];
+int[] currentArray2 = new int[length];
 
 Console.Clear();
-
+Console.WriteLine("***************************************************************************");
 Console.WriteLine("Дано количество единиц " + numberOnes);
 Console.WriteLine("Длина массива " + length);
+Console.WriteLine("**************************************************************************");
+
 FillArray(currentArray, numberOnes);
+FillArrayMath(currentArray2, numberOnes);
+
+Console.WriteLine("Заполняем случайно, пока есть единицы:");
 PrintArray(currentArray);
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Количество единиц случайно, заполнение по алгоритму:");
+PrintArray(currentArray2);
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
 
 void FillArray (int[] currentArray, int number1)
 {
@@ -32,5 +47,14 @@ void PrintArray (int[] currentArray)
     {
         Console.Write(currentArray[i] + "\t");
         if (((i + 1) % 10 == 0) && i > 1) Console.WriteLine();
+    }
+}
+
+void FillArrayMath (int[] currentArray, int number)
+{
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if (i % number == 0) currentArray[i] = 1;
+        else currentArray[i] = 0;
     }
 }
